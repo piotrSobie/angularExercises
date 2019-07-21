2e36589e62ca5fc547509e4495a0aaa86846b2ae
piotrSobie <piotrsobiegraj736@gmail.com>
2019-07-21 13:53:35 +0200

    exercise 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import {WarningAlertComponent} from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
